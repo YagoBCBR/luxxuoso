00fe7d4a9edf356c05c5bc327f93049175543354
yagodev <yagocampos11@hotmail.com>
2024-07-10 19:01:39 -0300

    alteração de rotas do menu de navegação do usuário logado

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -4,7 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../pages/Home';
 import Feed from '../pages/Feed';
 import ClubeDePontos from '../pages/ClubeDePontos';
-import NewPost from '../pages/NewPost';
+import Cursos from '../pages/Cursos';
 import Profile from '../pages/Profile';
 
 const Tab = createBottomTabNavigator();
@@ -12,10 +12,10 @@ const Tab = createBottomTabNavigator();
 function AppRoutes(){
     return(
         <Tab.Navigator>
-            <Tab.Screen name='Home' component={Home} />
             <Tab.Screen name='Feed' component={Feed} />
             <Tab.Screen name='Clube' component={ClubeDePontos} />
-            <Tab.Screen name='Postar' component={NewPost} />
+            <Tab.Screen name='Home' component={Home} />
+            <Tab.Screen name='Cursos' component={Cursos} />
             <Tab.Screen name='Perfil' component={Profile} />
         </Tab.Navigator>
     )
